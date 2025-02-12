0f5efb9fe60a66b58b48bfdd6658c4301da894e0
SilviyaDangol <sylvia199703@gmail.com>
2025-02-12 14:48:07 +0545

    one-time admin setup added

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const { pool } = require('../database/db');
 const userQueries = require('../database/queries/user.queries');
 const productQueries = require('../database/queries/product.queries');
 const { authenticate, authorize } = require('../middleware/auth');
+const bcrypt = require("bcryptjs");
 
 // User Management
 router.get('/users', authenticate, authorize('admin'), async (req, res) => {
@@ -13,6 +14,17 @@ router.get('/users', authenticate, authorize('admin'), async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
+router.put('/users', authenticate, authorize('admin'), async (req, res) => {
+    try {
+        const{username, password, email,role} = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await pool.query(`INSERT INTO users (username, email, password_hash, role)  VALUES ($1, $2, $3, $4)`, [username, email, hashedPassword,role]);
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
 
 router.delete('/users/:userId', authenticate, authorize('admin'), async (req, res) => {
     try {
@@ -54,6 +66,25 @@ router.delete('/products/:productId', authenticate, authorize('admin'), async (r
     try {
         await pool.query(productQueries.deleteProduct, [req.params.productId]);
         res.json({ message: 'Product deleted successfully' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+router.post('/oneTimeSetup', async (req, res) => {
+    // If user with role count is 0, create a default admin user
+    try {
+        const result = await pool.query("SELECT COUNT(id) from users WHERE role='admin' ");
+        if (parseInt(result.rows[0]['count']) === 0) {
+            const {adminUsername, adminPassword} = req.body;
+            const hashedPassword = await bcrypt.hash(adminPassword, 10);
+            await pool.query(`INSERT INTO users (username, email, password_hash, role)  VALUES ($1, $2, $3, 'admin')`, [adminUsername, adminUsername, hashedPassword]);
+            res.json({ message: 'Setup complete' });
+        } else {
+            res.status(403).json({ error: "OneTimeSetup already completed" });
+        }
+        console.log(result.rows[0]['count'])
+
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-
-
-
 const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin');
 const userRoutes = require('./routes/user');
 const cartRoutes = require('./routes/cart');
 const{authenticate, authorize} = require('./middleware/auth');
-const { initializeDatabase } = require('./database/db');
+const { initializeDatabase, pool} = require('./database/db');
 const { logger } = require('./helpers/logger.js');
 require('dotenv').config();
 
@@ -22,9 +19,10 @@ app.use(express.json());
 app.use(cors());
 //Routes
 app.use('/auth', authRoutes);
-app.use('/admin', authenticate, authorize('admin'), adminRoutes);
+app.use('/admin', adminRoutes);
 app.use('/users', userRoutes)
 app.use('/cart',authenticate, cartRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Listening on port: " + PORT));
